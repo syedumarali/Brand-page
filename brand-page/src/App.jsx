5d74dcdb11f38023dc1ae1c3bffdf1ff9fe7404f
syed umar ali <umaxkhan786@gmail.com>
2024-03-15 15:24:16 +0500

    brand-page

---
+++ brand-page/src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./components/Hero";
+import "./App.css";
+import Navigation from "./components/Navigation";
+
+const App=()=>{
+  return (<div>
+    <Navigation/>
+  <Hero/>
+  </div>
+);
+};
+export default App;
